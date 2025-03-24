ecfe5ebf0d17b6e8405df8431a2b61e487d55ff9
Routinecoin <support@routinecoin.info>
2025-03-24 08:09:17 +0530

    ROU Routinecoin.sol

--- Routinecoin.sol
+++ Routinecoin.sol
@@ -1,44 +1,38 @@
-pragma solidity ^0.5.17;
+pragma solidity ^ 0.5.17;
 import "safemath.sol";
 import "safeERC20.sol";
 contract RoutineCoin {
-	  using SafeMath for uint256;
-	  using SafeERC20 for IERC20;
-    IERC20 public token;
+	using SafeMath for uint256;
+        using SafeERC20 for IERC20;
+            IERC20 public token;
 
     string public constant name = "ROUTINE COIN";
-    string public constant symbol = "RTC";
+    string public constant symbol = "ROU";
     uint256 public constant decimals = 18;
-	  uint256 constant public INVEST_MIN_AMOUNT 	= 5000e18; // 100 USDT 
-      
-	  uint256 public maxUser = 280;
-	  uint256 public currentUser = 0;
-	  uint256 public totalInvested;
     address public stakeNode;        
     address public marketing;        
     address public partners; 
-    address public angleInvestors; 
-    address public liquidityPool; 
+    address public preMined; 
     address public reserve;  
-    address payable public mainAccount;
 
-    uint256 public constant maxTotalSupply          = 2800000000 * 10**decimals;   
-    uint256 public constant totalSupplyInc          = 2240000000 * 10**decimals;   
-    uint256 public constant stakeNodeFund           = 1960000000 * 10**decimals;   
-    uint256 public constant marketingFund           = 56000000 * 10**decimals;   
-    uint256 public constant partnersFund            = 84000000 * 10**decimals;   
-    uint256 public constant angleInvestorsFund      = 280000000 * 10**decimals;   
-    uint256 public constant liquidityPoolFund       = 280000000 * 10**decimals;   
-    uint256 public constant reserveFund             = 140000000 * 10**decimals;
+    uint256 public constant maxTotalSupply = 1790000000 * 10 ** decimals;   
+    uint256 public constant totalSupplyInc = 0 * 10 ** decimals;   
+    uint256 public constant stakeNodeFund = 1432000000 * 10 ** decimals;   
+    uint256 public constant marketingFund = 35800000 * 10 ** decimals;   
+    uint256 public constant partnersFund = 53700000 * 10 ** decimals;   
+    uint256 public constant preMinedFund = 179000000 * 10 ** decimals;   
+    uint256 public constant reserveFund = 89500000 * 10 ** decimals;
 
     uint256 public starttime;   
-    uint256 public constant marketingPerBlock       = 771604938000000000;
-    uint256 public constant partnersPerBlock        = 1157407410000000000;
-    uint256 public constant reservePerBlock         = 1929012350000000000;
+    uint256 public constant stakeNodePerBlock = 9075654710000000000;
+    uint256 public constant marketingPerBlock = 226891368000000000;
+    uint256 public constant partnersPerBlock = 340337052000000000;
+    uint256 public constant reservePerBlock = 567228419000000000;
+    uint256 public stakeNodeClaimed;   
     uint256 public marketingClaimed;   
     uint256 public partnersClaimed;   
-    uint256 public reserveClaimed;   
-    
+    uint256 public reserveClaimed;
+
     mapping(address => uint256) balances;
     mapping(address => mapping(address => uint)) allowed;
 
@@ -56,10 +50,10 @@ contract RoutineCoin {
     }
 
     struct UserList {
-      userListDeposit[] userListDeposits;
+        userListDeposit[] userListDeposits;
       uint256 total;
     }
-    mapping (uint256 => UserList) public userslist;
+    mapping(uint256 => UserList) public userslist;
 
     struct Deposit {
       uint256 time;
@@ -68,239 +62,167 @@ contract RoutineCoin {
     }
 
     struct User {
-      Deposit[] deposits;
+        Deposit[] deposits;
       uint256 checkpoint;
       uint256 withdrawn;
     }
-	  mapping (address => User) public users;
+    mapping(address => User) public users;
 
     // constructor
     constructor() public
     {
-      stakeNode         = 0x14fa12FC197eFdfe04F24809Ab72906Ac36CAFBE;
-      marketing         = 0x0cA0b15d5f180eb0a2d9624efF406c183fb09ee2;
-      partners          = 0x0Ca6e353144ef20d6206990Aa72f3bb2DdEb31ff;
-      liquidityPool     = 0x08b4aC443E6F4b114d3f474A00C69A400B996403;
-      reserve           = 0x91592cb1eE5B732EB80f15C5B4901D776d5D6E42;
-      mainAccount       = 0x189c3bA7E27050165C7A2F67a7865830192eC2c4;
-      token             = IERC20(0x55d398326f99059fF775485246999027B3197955);
-
-      balances[stakeNode]           = stakeNodeFund;        // Deposit tokens for Owners
-      balances[liquidityPool]       = liquidityPoolFund;    // Deposit tokens for Owners
-
-      emit Transfer(address(this), stakeNode, stakeNodeFund);
-      emit Transfer(address(this), liquidityPool, liquidityPoolFund);
-
-      starttime = block.timestamp;
+        stakeNode = 0x27440F8C09b07c3Fd84F8F789C00A681037284C5;
+        marketing = 0x75F5A65338475525BC6c4A778d0b308D3decC26F;
+        partners = 0x92AeadCb87AB163f30D2F950891D70Cd9A166bA2;
+        preMined = 0x74ED9f9b47b0788E53259dcaFBCf44dE28F4149C;
+        reserve = 0x6dC43a3BCa8a600dAF94EFb5A940303683dcFa34;
+        balances[preMined] = preMinedFund;        // Deposit tokens for Owners
+      emit Transfer(address(this), preMined, preMinedFund);
+        starttime = block.timestamp;
     }
 
-    function balanceOf(address _owner) view public returns (uint256 balance) {
-      return balances[_owner];  
+    function balanceOf(address _owner) view public returns(uint256 balance) {
+        return balances[_owner];
     }
 
-    function totalSupply() public view returns (uint256) {
-    	return totalSupplyInc+getTokenInc()+getTokenIncMarketing(0)+getTokenIncPartners(0)+getTokenIncReserve(0);
+    function totalSupply() public view returns(uint256) {
+        return preMinedFund + getTokenIncStakeNode(0) + getTokenIncMarketing(0) + getTokenIncPartners(0) + getTokenIncReserve(0);
     }
 
-    function maxSupply() public view returns (uint256) {
-    	return maxTotalSupply;
+    function maxSupply() public view returns(uint256) {
+        return maxTotalSupply;
     }
 
-    function invest(uint256 iamount) public payable{
-      uint256 value = iamount*10**18;
-      require(value == INVEST_MIN_AMOUNT);
-      require(value <= token.allowance(msg.sender, address(this)));
-      require(currentUser < maxUser);
-      uint256 time = 840;
-      
-      User storage user = users[msg.sender];
-      UserList storage userlist = userslist[0];
-
-      token.safeTransferFrom(msg.sender, mainAccount, value);
-
-      if (user.deposits.length == 0) {
-        user.checkpoint = block.timestamp;
-      }
-      userlist.total = userlist.total+1;
-
-      user.deposits.push(Deposit(time, value, block.timestamp));
-      userlist.userListDeposits.push(userListDeposit(time, value, block.timestamp));
-      currentUser = currentUser+1;
-      stakeBalanceLedger_[msg.sender] = SafeMath.add(stakeBalanceLedger_[msg.sender], value);
-      totalInvested = SafeMath.add(totalInvested, value);
-    }
-
-    function withdrawdividend() public {
-        User storage user = users[msg.sender];
-
-        uint256 totalAmount 		= getUserDividends(msg.sender);
-        user.withdrawn 				= user.withdrawn.add(totalAmount);
+    function withdrawStakeNode() public {
+        require(msg.sender == stakeNode);
+        uint256 totalAmount = getTokenIncStakeNode(1);
+        stakeNodeClaimed = stakeNodeClaimed.add(totalAmount);
         balances[msg.sender] = SafeMath.add(balances[msg.sender], totalAmount);
         emit Transfer(address(this), msg.sender, totalAmount);
     }
 
     function withdrawMarketing() public {
         require(msg.sender == marketing);
-        uint256 totalAmount 		= getTokenIncMarketing(1);
-        marketingClaimed 				= marketingClaimed.add(totalAmount);
+        uint256 totalAmount = getTokenIncMarketing(1);
+        marketingClaimed = marketingClaimed.add(totalAmount);
         balances[msg.sender] = SafeMath.add(balances[msg.sender], totalAmount);
         emit Transfer(address(this), msg.sender, totalAmount);
     }
 
     function withdrawPartners() public {
         require(msg.sender == partners);
-        uint256 totalAmount 		= getTokenIncPartners(1);
-        partnersClaimed 				= partnersClaimed.add(totalAmount);
+        uint256 totalAmount = getTokenIncPartners(1);
+        partnersClaimed = partnersClaimed.add(totalAmount);
         balances[msg.sender] = SafeMath.add(balances[msg.sender], totalAmount);
         emit Transfer(address(this), msg.sender, totalAmount);
     }
 
     function withdrawReserve() public {
         require(msg.sender == reserve);
-        uint256 totalAmount 		= getTokenIncMarketing(1);
-        reserveClaimed 				= reserveClaimed.add(totalAmount);
+        uint256 totalAmount = getTokenIncReserve(1);
+        reserveClaimed = reserveClaimed.add(totalAmount);
         balances[msg.sender] = SafeMath.add(balances[msg.sender], totalAmount);
         emit Transfer(address(this), msg.sender, totalAmount);
     }
 
-    function getTokenIncMarketing(uint256 user) public view returns (uint256) {
+    function getTokenIncStakeNode(uint256 user) public view returns(uint256) {
         uint256 totalAmount;
-        uint256 time = 840;
+        uint256 time = 1825;
+        uint256 finish = starttime.add(time.mul(1 days));
+        uint256 share = stakeNodePerBlock;
+        uint256 from = starttime;
+        uint256 to = finish < block.timestamp ? finish : block.timestamp;
+        if (from < to) {
+            totalAmount = totalAmount.add(share.mul(to.sub(from)));
+        }
+        if (user == 1) {
+            return totalAmount - stakeNodeClaimed;
+        } else {
+            return totalAmount;
+        }
+    }
+
+    function getTokenIncMarketing(uint256 user) public view returns(uint256) {
+        uint256 totalAmount;
+        uint256 time = 1825;
         uint256 finish = starttime.add(time.mul(1 days));
         uint256 share = marketingPerBlock;
         uint256 from = starttime;
         uint256 to = finish < block.timestamp ? finish : block.timestamp;
         if (from < to) {
-          totalAmount = totalAmount.add(share.mul(to.sub(from)));					
-        }	
-        if(user == 1) {
-          return totalAmount-marketingClaimed;
+            totalAmount = totalAmount.add(share.mul(to.sub(from)));
+        }
+        if (user == 1) {
+            return totalAmount - marketingClaimed;
         } else {
-          return totalAmount;
+            return totalAmount;
         }
     }
 
-    function getTokenIncPartners(uint256 user) public view returns (uint256) {
+    function getTokenIncPartners(uint256 user) public view returns(uint256) {
         uint256 totalAmount;
-        uint256 time = 840;
+        uint256 time = 1825;
         uint256 finish = starttime.add(time.mul(1 days));
         uint256 share = partnersPerBlock;
         uint256 from = starttime;
         uint256 to = finish < block.timestamp ? finish : block.timestamp;
         if (from < to) {
-          totalAmount = totalAmount.add(share.mul(to.sub(from)));					
-        }	
-        if(user == 1) {
-          return totalAmount-partnersClaimed;
+            totalAmount = totalAmount.add(share.mul(to.sub(from)));
+        }
+        if (user == 1) {
+            return totalAmount - partnersClaimed;
         } else {
-          return totalAmount;
+            return totalAmount;
         }
     }
 
-    function getTokenIncReserve(uint256 user) public view returns (uint256) {
+    function getTokenIncReserve(uint256 user) public view returns(uint256) {
         uint256 totalAmount;
-        uint256 time = 840;
+        uint256 time = 1825;
         uint256 finish = starttime.add(time.mul(1 days));
         uint256 share = reservePerBlock;
         uint256 from = starttime;
         uint256 to = finish < block.timestamp ? finish : block.timestamp;
         if (from < to) {
-          totalAmount = totalAmount.add(share.mul(to.sub(from)));					
-        }	
-        if(user == 1) {
-          return totalAmount-reserveClaimed;
+            totalAmount = totalAmount.add(share.mul(to.sub(from)));
+        }
+        if (user == 1) {
+            return totalAmount - reserveClaimed;
         } else {
-          return totalAmount;
+            return totalAmount;
         }
     }
 
-    function getTokenInc() public view returns (uint256) {
-        UserList storage userlist = userslist[0];
-        uint256 totalAmount;
-        for (uint256 i = 0; i < userlist.userListDeposits.length; i++) {			
-        uint256 finish = userlist.userListDeposits[i].start.add(userlist.userListDeposits[i].time.mul(1 days));
-
-        uint256 share = 13700000000000000;
-        uint256 from = userlist.userListDeposits[i].start;
-        uint256 to = finish < block.timestamp ? finish : block.timestamp;
-        if (from < to) {
-          totalAmount = totalAmount.add(share.mul(to.sub(from)));					
-        }	
-        
-      }
-      return totalAmount;
-    }
-
-    function getUserDividends(address userAddress) public view returns (uint256) {
-      User storage user = users[userAddress];
-        uint256 totalAmount;
-        for (uint256 i = 0; i < user.deposits.length; i++) {			
-        uint256 finish = user.deposits[i].start.add(user.deposits[i].time.mul(1 days));
-        if (user.checkpoint < finish) {
-          uint256 share = 13700000000000000;
-          uint256 from = user.deposits[i].start > user.checkpoint ? user.deposits[i].start : user.checkpoint;
-          uint256 to = finish < block.timestamp ? finish : block.timestamp;
-          if (from < to) {
-            totalAmount = totalAmount.add(share.mul(to.sub(from)));					
-          }	
-        }
-      }
-      return totalAmount-users[userAddress].withdrawn;
-    }
-
-    function getTotalDividend(address userAddress) public view returns(uint256, uint256) {
-      return (getUserDividends(userAddress), getUserDividends(userAddress)+users[userAddress].withdrawn);
-    }
-
-    function getUserAmountOfDeposits(address userAddress) public view returns(uint256) {
-      return users[userAddress].deposits.length;
-    }
-
-    function getUserTotalDeposits(address userAddress) public view returns(uint256 amount) {
-      for (uint256 i = 0; i < users[userAddress].deposits.length; i++) {
-        amount = amount.add(users[userAddress].deposits[i].amount);
-      }
-    }
-    
-    function getUserDepositInfo(address userAddress, uint256 index) public view returns(uint256 amount, uint256 start, uint256 finish) {
-      User storage user = users[userAddress];
-      uint256 time;
-      time = user.deposits[index].time;
-      amount = user.deposits[index].amount;
-      start = user.deposits[index].start;
-      finish = user.deposits[index].start.add(time.mul(1 days));
-    }
-
-    function transfer(address _to, uint256 _value) public returns (bool)
+    function transfer(address _to, uint256 _value) public returns(bool)
     {
-      require(_value <= balances[msg.sender]);
-      balances[msg.sender] = SafeMath.sub(balances[msg.sender], _value);
-      balances[_to] = SafeMath.add(balances[_to], _value);
+        require(_value <= balances[msg.sender]);
+        balances[msg.sender] = SafeMath.sub(balances[msg.sender], _value);
+        balances[_to] = SafeMath.add(balances[_to], _value);
       emit Transfer(msg.sender, _to, _value);
-      return true;
+        return true;
     }
 
-    function transferFrom( address _from, address _to, uint256 _amount ) public returns (bool success) {
-        require( _to != address(0));
+    function transferFrom(address _from, address _to, uint256 _amount) public returns(bool success) {
+        require(_to != address(0));
         require(balances[_from] >= _amount && allowed[_from][msg.sender] >= _amount && _amount >= 0);
-        balances[_from] = SafeMath.sub(balances[_from],_amount);
-        allowed[_from][msg.sender] = SafeMath.sub(allowed[_from][msg.sender],_amount);
-        balances[_to] = SafeMath.add(balances[_to],_amount);
+        balances[_from] = SafeMath.sub(balances[_from], _amount);
+        allowed[_from][msg.sender] = SafeMath.sub(allowed[_from][msg.sender], _amount);
+        balances[_to] = SafeMath.add(balances[_to], _amount);
         emit Transfer(_from, _to, _amount);
         return true;
     }
-    
+
     // Allow _spender to withdraw from your account, multiple times, up to the _value amount.
     // If this function is called again it overwrites the current allowance with _value.
-    function approve(address _spender, uint256 _amount) public returns (bool success) {
-        require( _spender != address(0));
+    function approve(address _spender, uint256 _amount) public returns(bool success) {
+        require(_spender != address(0));
         allowed[msg.sender][_spender] = _amount;
         emit Approval(msg.sender, _spender, _amount);
         return true;
     }
-  
-    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
-        require( _owner != address(0) && _spender !=address(0));
+
+    function allowance(address _owner, address _spender) public view returns(uint256 remaining) {
+        require(_owner != address(0) && _spender != address(0));
         return allowed[_owner][_spender];
     }
 
